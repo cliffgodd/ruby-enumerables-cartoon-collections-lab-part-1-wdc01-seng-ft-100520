414961412a61086f39394c1ebe7b2b77aac7af02
Justin Clifford <justoon7.jc@gmail.com>
2020-09-27 05:30:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |index, element|
+  array.each_with_index do |element, index|
     puts "#{index}. #{element}"
   end
 
